6ba531995616eaf2ce64b073e6ed57679cc2236c
Daniele Orlandi <daniele@orlandi.com>
2011-05-01 15:09:00 +0200

    Initial implementation of models merge functionality

--- app/models/pilot.rb
+++ app/models/pilot.rb
@@ -24,23 +24,11 @@ class Pilot < Ygg::PublicModel
   validates_uniqueness_of :person_id
 #  validates_presence_of :club
 
-  def steal_from_other_pilot(unlucky)
-    self.flights << unlucky.flights
-    self.planes << unlucky.planes
-    self.championship_pilots << unlucky.championship_pilots
-
-    self.person.birth_date = unlucky.person.birth_date if !self.person.birth_date
-    self.person.vat_number = unlucky.person.vat_number if !self.person.vat_number
-    self.person.italian_fiscal_code = unlucky.person.italian_fiscal_code if !self.person.italian_fiscal_code
-    self.person.home_location_id = unlucky.person.home_location_id if !self.person.home_location_id
-    self.person.birth_location_id = unlucky.person.birth_location_id if !self.person.birth_location_id
-    self.person.tmp_telefono = unlucky.person.tmp_telefono if !self.person.tmp_telefono
-    self.person.tmp_cellulare = unlucky.person.tmp_cellulare if !self.person.tmp_cellulare
-
-    self.person.identities << unlucky.person.identities
-
-    self.person.save!
-    self.save!
+  def merge(other)
+    flights << other.flights
+    planes << other.planes
+    championship_pilots << other.championship_pilots
+    save!
   end
 end
 
